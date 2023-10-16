816b74503bb53d40fdcea72e6682d11fed647a04
stevne.chao <gash612@gmail.com>
2023-10-16 11:05:50 +0800

    [Feat] Dropping ball and new ball generate

--- src/collisionHandle.js
+++ src/collisionHandle.js
@@ -1,5 +1,11 @@
 function collisionTriggered(e) {
     if (e.name == 'collisionStart' && e.pairs.length > 0) {
+        //Any collision for the dropping ball make the new ball generated
+        if ((e.pairs[0].bodyA.id == holdingBall.id || e.pairs[0].bodyB.id == holdingBall.id) && holdingDropping) {
+            holdingDropping = false;
+            holdingBall = createBall("top", getRandomNumber(1, 5));
+        }
+        //Deal ball bigger only when the same ball collision
         if (e.pairs[0].bodyA.label != e.pairs[0].bodyB.label) return;
         ballCollision(e.pairs[0].bodyA.label, e.pairs[0].bodyA.id, e.pairs[0].bodyB.id);
     }
