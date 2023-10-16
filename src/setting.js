6993af192aae7a23dde5ad7a83da9b666e4b95df
stevne.chao <gash612@gmail.com>
2023-10-16 15:41:20 +0800

    [Material] Add the dog ball material and tune their scale values

--- src/setting.js
+++ src/setting.js
@@ -5,4 +5,4 @@ var setting_usingBallImage = true;
 //If ball use the image, image too big or too small can use this value to change(Ball radius in game is fixed, wont be affected by this)
 //-1 is the no use slot, please edit after that
 //ball radius ratio for reference [-1, 0.08, 0.1, 0.14, 0.16, 0.18, 0.24, 0.32, 0.40, 0.48, 0.56, 0.64];
-var setting_textureScale = [-1, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75];
+var setting_textureScale = [-1, 0.04, 0.06, 0.08, 0.105, 0.108, 0.142, 0.21, 0.24, 0.3, 0.3, 0.435];
